26656bf52b6cd2ff4df7ad7b27f2d2d0c68da37f
Rafael Lima Barreto <rafaellbecom@gmail.com>
2024-02-16 21:11:31 -0300

    Refatorando requisição com  Async/Await e tratando erros

--- script.js
+++ script.js
@@ -1,22 +1,27 @@
 const containerVideos = document.querySelector('.videos__container');
 
-const api = fetch('http://localhost:3000/videos');
-const resp = api.then(resp => resp.json());
-resp.then((videos) => {
-    videos.forEach(video => {
-        containerVideos.innerHTML += `
-            <li class="videos__item">
-                <iframe src="${video.url}" title="${video.titulo}" frameborder="0" allowfullscreen></iframe>
-                <div class="descricao-video">
-                    <img class="img-canal" src="${video.imagem}" alt="Logo do Canal">
-                    <h3 class="titulo-video">${video.titulo}</h3>
-                    <p class=""titulo-canal">${video.descricao}</p>
-                </div>
-            </li>
-        `;
-    });
-}).catch((error) => {
-    containerVideos.innerHTML = `
-        <p>Ouve um erro ao carregar os vídeo: ${error}</p>
-    `;
-})
+async function buscarEMostrarVideos() {
+    try{
+        const busca = await fetch('http://localhost:3000/videos');
+        const videos = await busca.json();
+        videos.forEach(video => {
+            if(video.url == ''){
+                throw new Error('Um dos vídeos não possui URL');
+            };
+            containerVideos.innerHTML += `
+                <li class="videos__item">
+                    <iframe src="${video.url}" title="${video.titulo}" frameborder="0" allowfullscreen></iframe>
+                    <div class="descricao-video">
+                        <img class="img-canal" src="${video.imagem}" alt="Logo do Canal">
+                        <h3 class="titulo-video">${video.titulo}</h3>
+                        <p class=""titulo-canal">${video.descricao}</p>
+                    </div>
+                </li>
+            `;
+        });
+    } catch(error){
+        containerVideos.innerHTML = `<p>Houve um erro ao carregar os vídeos: ${error}</p>`
+    }
+};
+
+buscarEMostrarVideos();
